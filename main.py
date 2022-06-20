6fd2d9483f36f57b73b2b356ee0cb96c2c556115
rohinirode <105968428+rohinirode@users.noreply.github.com>
2022-06-21 00:13:16 +0530

    added code

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# This program sub of two numbers
+
+num1 =6
+num2 = 3
+
+# Add two numbers
+sum = num1 - num2
+
+# Display the sub
+print('The sub of {0} and {1} is {2}'.format(num1, num2, sum))
